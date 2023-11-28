56ec53716ee5cc0ecd342e84fc38567b08e79210
Francisco-Diaz-Roldan <pacodiaz008@gmail.com>
2023-11-28 12:06:08 +0100

    Boton volver en la aplicacion de saludo.

--- app/src/main/java/com/example/primeritaappconaristidevs/firstapp/ResultActivity.kt
+++ app/src/main/java/com/example/primeritaappconaristidevs/firstapp/ResultActivity.kt
@@ -3,10 +3,14 @@ package com.example.primeritaappconaristidevs.firstapp
 import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
 import com.example.primeritaappconaristidevs.R
 
 class ResultActivity : AppCompatActivity() {
+
+    private lateinit var btnVolver: Button
+
     @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -14,5 +18,8 @@ class ResultActivity : AppCompatActivity() {
         val tvResult = findViewById<TextView>(R.id.tvResult)
         val name: String = intent.extras?.getString("EXTRA_NAME").orEmpty()
         tvResult.text = "Hola $name"
+
+        btnVolver = findViewById(R.id.btnVolver)
+        btnVolver.setOnClickListener{ onBackPressed() }
     }
 }
